8a17a317fe4809a064b52f4dadb3ab05fc90cfc6
Dustin Lee <leedustin@live.com>
2023-02-16 18:14:03 -0500

    added portfolio images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
-export default function Navbar(props) {
+export default function Navbar({page, handlePageRender}) {
     return (
         <nav className='nav-card'>
-          <h1><a onClick={() => props.handlePageRender('home') } href='#home'>Dustin Lee</a></h1>
+          <h1><a onClick={() => handlePageRender('home') } href='#home'>Dustin Lee</a></h1>
           <ul className="nav-links">
-            <li className="nav-item"><a onClick={() => props.handlePageRender('about-me') } href='#about-me'>About Me</a></li>
-            <li className="nav-item"><a href='#portfolio'>Portfolio</a></li>
+            <li className="nav-item"><a onClick={() => handlePageRender('about-me') } href='#about-me'>About Me</a></li>
+            <li className="nav-item"><a onClick={() => handlePageRender('portfolio') } href='#portfolio'>Portfolio</a></li>
             <li className="nav-item"><a href='#contact'>Contact</a></li>
             <li className="nav-item"><a href='#resume'>Resume</a></li>
           </ul>
