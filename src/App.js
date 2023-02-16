c00521bd1d349abc73ee51782327a3995fae4bfa
Dustin Lee <leedustin@live.com>
2023-02-16 17:56:59 -0500

    home and about page added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
+// pages
+import Home from './components/pages/Home'
+import AboutMe from './components/pages/AboutMe'; 
+
+// components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
+  // use state to render page when nav links clicked
+  const [page, setPage] = useState('home');
+
+  const handlePageRender = page => {
+    // console.log('handlepage render func hit: ')
+    setPage(page);
+  }
+
+  const renderPage = () => {
+    if (page === "home") {
+      return <Home page={page} handlePageRender={handlePageRender} />
+    } else if (page === "about-me") {
+      return <AboutMe />
+    } 
+
+  }
+
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        // <p>
-        //   Edit <code>src/App.js</code> and save to reload.
-        // </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+    <div>
+      <Navbar page={page} handlePageRender={handlePageRender} />
+      <div id='main-card'>
+        {renderPage()}
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <div className="footer-card">
+            <ul className="footer-links">
+                <li className="footer-item"><a href="mailto: leedustin@live.com">Email Me</a></li>
+                <li className="footer-item"><a href='https://github.com/DustinLee0'>Github</a></li>
+                {/* no linked in url given */}
+                <li className="footer-item"><a href="#">LinkedIn</a></li>
+            </ul>
+        </div>
+    )
+}
