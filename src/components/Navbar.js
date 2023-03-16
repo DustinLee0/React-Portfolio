9d67a955ff383ae2a6753dd2b58f29047950c718
Dustin Lee <leedustin@live.com>
2023-03-15 20:41:31 -0400

    changed rerender of main section to use react router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Navbar({ page, handlePageRender }) {
   return (
     <header className="nav">
       <nav className='nav-card'>
-        <h1><a onClick={() => handlePageRender('home')} href='#home'>Dustin Lee</a></h1>
+        <h1><Link to="/">Dustin Lee</Link></h1>
         <ul className="nav-links">
-          <li className="nav-item"><a onClick={() => handlePageRender('about-me')} href='#about-me'>About Me</a></li>
-          <li className="nav-item"><a onClick={() => handlePageRender('projects')} href='#projects'>Projects</a></li>
-          <li className="nav-item"><a onClick={() => handlePageRender('contact')}href='#contact'>Contact</a></li>
+          <li className="nav-item"><Link to="/about-me">About Me</Link></li>
+          <li className="nav-item"><Link to="/projects">Projects</Link></li>
+          <li className="nav-item"><Link to="/contact">Contact Me</Link></li>
           {/* TO BE ADDED */}
           {/* <li className="nav-item"><a href='#resume'>Resume</a></li> */}
         </ul>
